195cbce3a17bd20c312151413ebafe12fff0eb6f
Shubham Ginde <shubhginde@gmail.com>
2022-07-14 15:06:00 +0530

    car details component

--- force-app/main/default/lwc/carTile/carTile.js
+++ force-app/main/default/lwc/carTile/carTile.js
@@ -6,4 +6,16 @@ export default class CarTile extends LightningElement {
     get carBrandandModel() {
         return this.car.Brand__r.Name + ' ' + this.car.Model__r.Name;
     }
+
+    get carPrice() {
+        return (this.car.Price__c / 100000).toPrecision(2) + ' Lakh';
+    }
+    get kmsDriven() {
+        return this.car.Kms_Driven__c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    handleShowModal() {
+        const modal = this.template.querySelector("c-car-details-popup");
+        modal.show();
+    }
 }
